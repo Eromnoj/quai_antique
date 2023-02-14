06e85892f3a340dc56389d793191c12cd5401df2
Jonathan Moreschi <j.moreschi@oulook.fr>
2023-02-14 17:35:46 +0100

    toggle button

--- assets/toggleNav.js
+++ assets/toggleNav.js
@@ -3,10 +3,9 @@ const link = document.querySelector('.link_list')
 const burgerLabel = document.querySelector('.burger')
 let isActive = false
 
+link.style.transform = isActive ? 'translateY(0px)' : 'translateY(-400px)'
 if (window.innerWidth < 875) {
   isActive = false
-  link.style.transform = isActive ? 'translateY(0px)' : 'translateY(-400px)'
-
 }
 
 window.addEventListener('resize', ()=>{
